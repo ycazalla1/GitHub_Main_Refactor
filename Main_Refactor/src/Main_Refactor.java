9f707fbfb438daa1be5e766abe1a73f29fda7189
Yamila <ycazalla1@gmail.com>
2025-02-18 17:58:36 +0100

    Primera versió del codi.

---
+++ Main_Refactor/src/Main_Refactor.java
@@ -0,0 +1,18 @@
+
+public class Main_Refactor {
+
+public static void main(String[] args) {
+		
+		int n = 5;
+		
+		int suma = 0;
+		
+		for (int i = 1; i <= n; i++) {
+			suma += i;
+		}
+		
+		System.out.println("La suma dels primers " + n + " números és: " + suma);
+		
+	}
+	
+}
